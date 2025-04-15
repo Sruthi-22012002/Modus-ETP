b2918c823125386624842cf762ac82844f9757d5
Sruthi-22012002 <sruthivg22@gmail.com>
2025-04-15 13:07:18 +0530

    app.js

--- sample-node/app.js
+++ sample-node/app.js
@@ -1,15 +1,15 @@
-// app.js
-
 const express = require('express');
 const app = express();
-const port = 3000;
+
+// Use the port Azure provides or fallback to 3000 for local testing
+const port = process.env.PORT || 3000;
 
 // Define the route for the homepage
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// Start the server on port 3000
+// Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
